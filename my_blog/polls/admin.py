5612b5eff6db25a441c5ce77dd84eb8d287e4a12
Tomasz Kudraszow <tomasz.kudraszow@infotech.edu.pl>
2022-10-10 10:31:46 +0200

    aaa

--- my_blog/polls/admin.py
+++ my_blog/polls/admin.py
@@ -8,7 +8,7 @@ class QuestionAdmin(admin.ModelAdmin):
     search_fields = ('question_text',)
 @admin.register(Choice)
 class ChoiceAdmin(admin.ModelAdmin):
-   list_display = ('get_question', 'choice_text', 'votes',)
+   list_display = ('get_question', 'choice_text', 'votes')
    list_filter = ('question', 'votes',)
    search_fields = ('choice_text', 'question__question_text',)
    raw_id_fields = ('question',)
--- my_blog/polls/views.py
+++ my_blog/polls/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Question
 
-# Create your views here.
+
+def question_list(request):
+   questions = Question.objects.all()
+   return render(request, 'polls/question_list.html', {'questions': questions})
+from .models import Question
+from django.shortcuts import render, get_object_or_404
+
+def question_list(request):
+   questions = Question.objects.all()
+   return render(request, 'polls/question_list.html', {'questions':
+questions})
+
+def question_detail(request, question_id):
+   question = get_object_or_404(Question, pk=question_id)
+   return render(request, 'polls/question_detail.html', {'question': question})
+   question = get_object_or_404(Question,pk=Question_id)
